c03e960222b3f9d978d62d93ce0ccdda281aeba1
Swapnaroop <swapnaroop2001@gmail.com>
2023-11-15 00:21:30 -0500

    Implemented doubly linkedlist successfully

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,113 +1,60 @@
-// /**
-//  * DoublyLinkedList
-//  */
-// public class DoublyLinkedList {
-//     Node head;
-
-//     /**
-//      * Node
-//      */
-//     public static class Node {
-//         Node prev;
-//         int data;
-//         Node next;
-
-//         Node(int data){
-//             this.data=data;
-//             this.prev=null;
-//             this.next=null;
-//         }
-//     }
-
-//     public void addNode(DoublyLinkedList l ,int d) {
-//         Node new_Node=new Node(d);
-//         if (head==null) {
-//             head=new_Node;
-//         }else{
-//             Node currn=head;
-//             while (currn.next!=null) {
-//                 currn.prev=currn;
-//                 currn=currn.next;
-//             }
-//             currn.next=new_Node;
-//         }
-//     }
-
-//     public void findLentgh(Node head) {
-//         int count=1;
-//         if(head==null){
-//             System.out.println("LinkedList is empty");
-//         }
-//         else{
-//             Node currn=head;
-//             while(currn.next!=null){
-//                 count+=1;
-//                 currn=currn.next;
-//             }
-//             System.out.println(count);
-//         }
-//     }
-
-//     public void getNthNode(int n) {
-//         if (head) {
-
-//         }
-//     }
-//     public static void main(String[] args) {
-//         Node a=new Node(10);
-//         Node b=new Node(20);
-//         Node c=new Node(30);
-//         Node d=new Node(40);
-
-//         a.prev=null;
-//         a.next=b;
-//         b.prev=a;
-//         b.next=c;
-//         c.prev=b;
-//         c.next=d;
-//         d.prev=c;
-//         d.next=null;
-
-//         System.out.println(a.next.next.prev.data);
-//         DoublyLinkedList l=new DoublyLinkedList();
-//         l.addNode(l, 10);
-//         l.addNode(l, 20);
-//         l.addNode(l, 30);
-//         l.addNode(l, 40);
-//         l.addNode(l, 50);
-//         l.addNode(l, 60);
-//         l.addNode(l, 70);
-
-//         l.findLentgh(l.head);
-
-//     }
-
-// }
-
 /**
  * DoublyLinkedList
  */
-
 public class DoublyLinkedList {
     Node head;
     Node tail;
 
-    public DoublyLinkedList() {
-        this.head = null;
-        this.tail = null;
-    }
-
-    public class Node {
-        int data;
+    /**
+     * Node
+     */
+    public static class Node {
         Node prev;
+        int data;
         Node next;
 
-        public Node(int data) {
+        Node(int data) {
             this.data = data;
             this.prev = null;
             this.next = null;
         }
     }
+
+    public void addNode(DoublyLinkedList l, int d) {
+        Node new_Node = new Node(d);
+        if (head == null) {
+            head=new_Node;
+            tail=new_Node;
+        } else {
+            tail= head;
+            while (tail.next!=null) {
+                tail=tail.next;
+            }
+            tail.next=new_Node;
+            new_Node.prev=tail;
+            tail=new_Node;
+        }
+    }
+
+    public void findLentgh(Node head) {
+        int count = 1;
+        if (head == null) {
+            System.out.println("LinkedList is empty");
+        } else {
+            Node currn = head;
+            while (currn.next != null) {
+                count += 1;
+                currn = currn.next;
+            }
+            System.out.println(count);
+        }
+    }
+
+    // public void getNthNode(int n) {
+    //     if (head) {
+
+    //     }
+    // }
 
     public void traverseForward() {
         Node current = head;
@@ -125,32 +72,21 @@ public class DoublyLinkedList {
         }
     }
 
-    public void addNode(DoublyLinkedList l ,int d) {
-                Node new_Node=new Node(d);
-                if (head==null) {
-                    head=new_Node;
-                }else{
-                    Node currn=head;
-                    while (currn.next!=null) {
-                        currn.prev=currn;
-                        currn=currn.next;
-                    }
-                    currn.next=new_Node;
-                }
-            
-    
-}
     public static void main(String[] args) {
-        DoublyLinkedList l=new DoublyLinkedList();
+        DoublyLinkedList l = new DoublyLinkedList();
         l.addNode(l, 10);
         l.addNode(l, 20);
         l.addNode(l, 30);
         l.addNode(l, 40);
         l.addNode(l, 50);
         l.addNode(l, 60);
+        l.addNode(l, 70);
 
         
+       
         l.traverseBackward();
-        System.out.println(l.tail.prev);
 
-}}
+    }
+
+}
+
